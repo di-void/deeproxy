2cf3477fb8bd57e2286506a3fa4915c6fa97d9e1
DEE <donzydonathan@gmail.com>
2025-01-25 23:12:26 +0100

    chore: set things up for server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,1 +1,17 @@
-//
+// https://docs.rs/hyper/latest/hyper/ 
+// https://hyper.rs/guides/1/server/hello-world/
+
+struct Server {
+    port: u16,
+    origin: String,
+}
+
+impl Server {
+    fn new(port: 16, origin: String) -> Self {
+        Self { port, origin }
+    }
+
+    fn start() {
+        todo!()
+    }
+}
