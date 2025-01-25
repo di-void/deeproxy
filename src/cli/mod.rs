5cae357034799757106e8f575017e29829b9ece1
DEE <donzydonathan@gmail.com>
2025-01-25 22:59:04 +0100

    chore: move cli stuff to mod file

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,2 +1,38 @@
-pub mod commands;
-pub use commands::*;
+use clap::{command, value_parser, arg, ArgMatches};
+
+// https://docs.rs/clap/latest/clap/index.html
+
+pub struct CLI {
+    args: ArgMatches,
+}
+
+pub enum Command {
+    ClearCache,
+    StartServer(u16, String)
+}
+
+impl CLI {
+    pub fn init() -> Self {
+        let matches = command!()
+            .arg_required_else_help(true)
+            .about("Deez Caching Proxy..")
+            .subcommand(command!("clear-cache").about("Clear the cache entirely"))
+            .arg(arg!(--port [PORT] "Sets the port the server should start on").default_value("3000").requires("origin").value_parser(value_parser!(u16)))
+            .arg(arg!(--origin <ORIGIN> "Sets the origin endpoint to proxy").value_parser(value_parser!(String)))
+            .get_matches();
+
+        Self { args: matches }
+    }
+
+    pub fn command(&self) -> Command {
+        match self.args.subcommand() {
+            Some(("clear-cache", _s)) => Command::ClearCache,
+            _ => {
+                let port = self.args.get_one::<u16>("port").unwrap();
+                let origin = self.args.get_one::<String>("origin").unwrap().clone();
+
+                Command::StartServer(*port, origin)
+            }
+        }
+    }
+}
